462f09b34088a59b4151e566ebdb78f99f97a9ff
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2023-11-03 15:05:07 -0700

    Nav bar complete & change play & download button border color

--- Netflix Clone/Views/HeroHeaderUIView.swift
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -12,7 +12,7 @@ class HeroHeaderUIView: UIView {
     private let downloadButton: UIButton = {
         let button = UIButton()
         button.setTitle("Download", for: .normal)
-        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
         button.layer.borderWidth = 1
         button.layer.cornerRadius = 5
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -22,7 +22,7 @@ class HeroHeaderUIView: UIView {
     private let playButton: UIButton = {
         let button = UIButton()
         button.setTitle("Play", for: .normal)
-        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderColor = UIColor.white.cgColor
         button.layer.borderWidth = 1
         button.layer.cornerRadius = 5
         button.translatesAutoresizingMaskIntoConstraints = false
